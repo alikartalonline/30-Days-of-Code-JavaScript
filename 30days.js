fb5e83d9953cb76dfb62a74b0aa30e96a2181b3d
Ali Kartal <ali_kartal91@hotmail.com>
2022-01-23 17:23:26 +0300

    Day 4: Class vs. Instance

--- 30days.js
+++ 30days.js
@@ -114,8 +114,35 @@ function main() {
 }
 
 
+// *************  Day 4: Class vs. Instance  ************* 
 
+function Person(initialAge){
 
+    // Add some more code to run some checks on initialAge
+    if( initialAge < 0) {
+        age = 0;
+        console.log("Age is not valid, setting age to 0.");
+    }
+    else if ( initialAge > 0) {
+        age = initialAge;
+    }
+    
+  this.amIOld=function(){
+   // Do some computations in here and print out the correct statement to the console 
+   if (age < 13) {
+       console.log("You are young.")
+   } 
+   else if (age >= 13 && age < 18){
+       console.log("You are a teenager.")
+   } 
+   else console.log("You are old.")
+};
+  
+   this.yearPasses=function(){
+          // Increment the age of the person in here
+          age += 1;
+   };
+}
 
 
 
